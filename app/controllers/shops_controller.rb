d8620df127ff4bf174ce15310cbe2b9f72377767
haruyamataiki <haruyamataiki619@gmali.com>
2020-03-02 10:05:18 +0000

    wip

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,18 @@
 class ShopsController < ApplicationController
   def index
+    @shops=Shop.all
+  	@shop = Shop.new
+    @user = current_user
+  end
+
+  def show
+	@shop = Shop.find(params[:id])
+    @Shop = Shop.new
+    @user = @Shop.user
+  end
+
+ private
+  def shop_params
+    params.require(:shop).permit(:shop_name,:caption)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def index
+      @users = User.all
+      @shop = Shop.new
+      @user = User.find(current_user.id)
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  	@shops = @user.shops
+    @shop = Shop.new
   end
 end
